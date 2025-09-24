c2a1d9bdba987f9cf17ae9130054c0bbdb1369fa
yuqizhang <yuqizhang247@gmail.com>
2025-09-24 16:52:59 -0400

    2nd commit

---
+++ SQD/__init__.py
@@ -0,0 +1,5 @@
+# --*-- conding:utf-8 --*--
+# @time:9/24/25 16:18
+# @Author : Yuqi Zhang
+# @Email : yzhan135@kent.edu
+# @File:__init__.py.py
---
+++ SQD/sqd_test.py
@@ -0,0 +1,7 @@
+# --*-- conding:utf-8 --*--
+# @time:9/24/25 16:18
+# @Author : Yuqi Zhang
+# @Email : yzhan135@kent.edu
+# @File:sqd_test.py
+
+
